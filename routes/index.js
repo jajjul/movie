990ce8cb9e411b2d317e6634b5a91dcdcfca60ce
jychoi <jychoi925@naver.com>
2020-04-14 02:23:35 +0900

    리스트 걸고 자잘한 css수정

--- routes/index.js
+++ routes/index.js
@@ -5,14 +5,26 @@ var router = express.Router();
 router.get('/', function (req, res, next) {
   res.render('index', {
     title: '영화토크방', list: [{
+      type: "리뷰",
+      contentTitle: "국제시장 리뷰",
+      commentCount: "1",
       author: "박태영",
-      name: "국제시장"
+      date: "2020.04.13",
+      voteCount: "25"
     }, {
-      author: "박태영",
-      name: "국제시장"
+      type: "자유",
+      contentTitle: "박태영 잘생겼음",
+      commentCount: "3",
+      author: "김마둠",
+      date: "2020.04.13",
+      voteCount: "0"
     }, {
-      author: "박태영",
-      name: "국제시장"
+      type: "미국 영화",
+      contentTitle: "미국영화 하나 추천함",
+      commentCount: "2",
+      author: "염다미",
+      date: "2020.04.13",
+      voteCount: "10"
     }]
   });
 });
@@ -21,14 +33,23 @@ router.get('/', function (req, res, next) {
   let category = req.params.category;
   res.render('index', {
     title: '영화토크방', list: [{
+      type: "리뷰",
+      contentTitle: "국제시장 리뷰",
       author: "박태영",
-      name: "국제시장"
+      date: "2020.04.13",
+      voteCount: "25"
     }, {
-      author: "박태영",
-      name: "국제시장"
+      type: "자유",
+      contentTitle: "박태영 잘생겼음",
+      author: "김마둠",
+      date: "2020.04.13",
+      voteCount: "0"
     }, {
-      author: "박태영",
-      name: "국제시장"
+      type: "미국 영화",
+      contentTitle: "미국영화 하나 추천함",
+      author: "염다미",
+      date: "2020.04.13",
+      voteCount: "10"
     }]
   });
 });
--- routes/movies.js
+++ routes/movies.js
@@ -2,16 +2,23 @@ var express = require('express');
 var router = express.Router();
 
 /* index.ejs <form action="movies"> */
-router.get('/', function(req, res, next) {
-  let search = req.query.search;
-  let target = req.query.target;
-  res.render('movie', { search: search, target: target });
-});
-
-router.post('/', function(req, res, next) {
-  let search = req.query.search;
-  let target = req.query.target;
-  res.render('movie', { search: search, target: target });
+router.get('/', function (req, res, next) {
+  res.render('movie', {
+    list: [
+      {
+        id: 1,
+        name: "abc"
+      },
+      {
+        id: 2,
+        name: "abc"
+      },
+      {
+        id: 3,
+        name: "abc"
+      }
+    ]
+  });
 });
 
 module.exports = router;
