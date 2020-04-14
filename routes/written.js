ff94473c820a0573b58a7ee3df6de421003839f8
jychoi <jychoi925@naver.com>
2020-04-15 06:20:33 +0900

    회원가입 페이지 만들고 기존 화면 자잘한 것들 수정

--- routes/written.js
+++ routes/written.js
@@ -3,7 +3,30 @@ var router = express.Router();
 
 
 router.get('/', function (req, res, next) {
-    res.render('written', {title: '글제목'});
+    res.render('written', {
+      title: '영화토크방', list: [{
+        type: "리뷰",
+        contentTitle: "국제시장 리뷰",
+        commentCount: "1",
+        author: "박태영",
+        date: "2020.04.13",
+        voteCount: "25"
+      }, {
+        type: "자유",
+        contentTitle: "박태영 잘생겼음",
+        commentCount: "3",
+        author: "김마둠",
+        date: "2020.04.13",
+        voteCount: "0"
+      }, {
+        type: "미국 영화",
+        contentTitle: "미국영화 하나 추천함",
+        commentCount: "2",
+        author: "염다미",
+        date: "2020.04.13",
+        voteCount: "10"
+        }]
+    });
 });
 
 module.exports = router;
